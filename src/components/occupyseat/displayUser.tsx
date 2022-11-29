483e7c64d6017472ebb844f06e4928d418f84d13
arshptl <arshpatel714@gmail.com>
2022-11-30 02:55:59 +0530

    added: button while not occupied

--- src/components/occupyseat/displayUser.tsx
+++ src/components/occupyseat/displayUser.tsx
@@ -1,15 +1,26 @@
-import React from 'react'
-import { OccupiedSeatType } from '../../utils/interfaces';
-import { Card, Title } from '@pankod/refine-antd'
+import React from "react";
+import { OccupiedSeatType } from "../../utils/interfaces";
+import { Button, Card } from "@pankod/refine-antd";
 
 const DisplayUser = (props: OccupiedSeatType): JSX.Element => {
-    return (
-        <div>
-            {props.userName}
-            {/* <Card >
+  console.log(props.occupied);
+  return (
+    <div>
+      <Card
+        style={{
+          width: "100%",
+          backgroundColor: "var(--primary-light)",
+          color: "var(--primary)",
+          margin: "0.5rem 0"
+        }}
+      >
+        Seat Number: {props.name}
+      </Card>
+      {!props.occupied && <Button title="Occupy Seat" type="primary">Occupy Seat</Button>}
+      {/* <Card >
             </Card> */}
-        </div>
-    )
-}
+    </div>
+  );
+};
 
-export default DisplayUser
+export default DisplayUser;
