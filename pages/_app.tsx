ca76c3e6f8f0ea96750cdfb0298e231ed3457656
arshptl <arshpatel714@gmail.com>
2022-12-07 04:33:30 +0530

    tried to implement graphql in refine

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,9 @@ import {
   ErrorComponent,
 } from "@pankod/refine-antd";
 import routerProvider from "@pankod/refine-nextjs-router";
-import dataProvider from "@pankod/refine-simple-rest";
+// import dataProvider from "@pankod/refine-simple-rest";
+import dataProvider, { GraphQLClient } from "@pankod/refine-graphql";
+
 require("antd/dist/antd.less");
 
 import {
@@ -25,13 +27,16 @@ import { UserShow, UserList } from "@components/users";
 import { OccupySeat, CreateSeat } from "@components/occupyseat";
 
 // const API_URL = "https://api.fake-rest.refine.dev";
-const API_URL = "http://localhost:3000";
+// const API_URL = "http://localhost:3000";
+const API_URL =
+  "https://878c-2401-4900-1f3e-6222-8c7b-64fc-3296-c09c.in.ngrok.io/pg/graphql";
+const client = new GraphQLClient(API_URL);
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <Refine
       routerProvider={routerProvider}
-      dataProvider={dataProvider(API_URL)}
+      dataProvider={dataProvider(client)}
       notificationProvider={notificationProvider}
       ReadyPage={ReadyPage}
       catchAll={<ErrorComponent />}
@@ -43,19 +48,19 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
       OffLayoutArea={OffLayoutArea}
       resources={[
         {
-          name: "users List",
+          name: "userlist",
           list: PostList,
           create: PostCreate,
           edit: PostEdit,
           show: PostShow,
         },
         {
-          name: "User Profile",
+          name: "userprofile",
           list: UserList,
           show: UserShow,
         },
         {
-          name: "occupy seat",
+          name: "occupyseat",
           list: OccupySeat,
           create: CreateSeat,
         },
